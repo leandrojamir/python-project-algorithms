b990cd5827284909d351b17a3e5339da83c587c3
leandrojamir <leandrojamir@hotmail.com>
2023-04-25 14:38:52 -0300

    start project

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,5 @@
+# iniciando projeto
+# 1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     raise NotImplementedError
