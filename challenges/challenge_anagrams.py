b0f701106ffdc8885c4527ecf6274464c1678487
leandrojamir <leandrojamir@hotmail.com>
2023-05-25 17:33:44 -0300

    fix: splitting function my_sorte to behave with asymptotic linearithmic complexity O(n log n)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,7 +7,10 @@ def is_anagram(first_string, second_string):
     # durante a comparação das entradas, ou seja, ser case insensitive.
     first_sort = my_sort(list(first_string.lower()))
     second_sort = my_sort(list(second_string.lower()))
+    # print(f"\nvvv\nprimeira string: {first_sort}")
+    # primeira string: ['a', 'd', 'e', 'p', 'r']
     first = "".join(first_sort)
+    print(f"\nvvv\nprimeira string: {first}")
     second = "".join(second_sort)
 
     #  Faça um algoritmo que consiga comparar duas strings, ordená-las e
@@ -30,15 +33,49 @@ def is_anagram(first_string, second_string):
 # Exemplos de funções não permitidas: sort, sorted e Counter;
 # Não será permitido realizar nenhuma importação neste arquivo!
 # https://www.programiz.com/dsa/selection-sort
+
+#  AssertionError: Seu algoritmo parece ser O(n^2), mas deveria ser
+# no máximo O(n log n)
+#  assert False
+
+# simplificar my_sort, dividir a lista em duas metades para ordenar e depois
+# mesclar com merge_sort com objetivo de diminuir algoritimo quadratico
 def my_sort(array):
-    size = len(array)
-    for index in range(size - 1):
-        min_index = index
+    # se menor que 1 já vai estar ordenado automaticamente
+    if len(array) <= 1:
+        return array
 
-        for info in range(index + 1, size):
-            if array[info] < array[min_index]:
-                min_index = info
+    # dividir a lista na metade esquerda e direita
+    mid = len(array) // 2
+    left_half = array[:mid]
+    right_half = array[mid:]
 
-        array[index], array[min_index] = (array[min_index], array[index])
+    # vou chamar recursivamente até metade esquerda ou direita ser menor que 1
+    left_half = my_sort(left_half)
+    right_half = my_sort(right_half)
 
-    return array
+    #  depois merge para juntar as duas metades ordenadas e retornar o array
+    # ordenado
+    sorted_array = merge(left_half, right_half)
+    return sorted_array
+
+
+# https://www.alura.com.br/artigos/algoritmo-mergesort-implementar-python?gclid=CjwKCAjw67ajBhAVEiwA2g_jEIYhRTgU0RULuMF8zrNZIdAQHVi1BvPG0IxU4Ruyg41dYoWVtb-EaRoCY0AQAvD_BwE
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    if left_index < len(left):
+        result.extend(left[left_index:])
+    else:
+        result.extend(right[right_index:])
+
+    return result
